73e5b3e41db1b1263a76297b3fabe54cc1ae7ef2
Shafi <jonyszone@gmail.com>
2023-11-26 21:57:09 +0600

    hotfix page identified

--- app/src/main/java/com/shafi/conposetestapp/home/Favorite.kt
+++ app/src/main/java/com/shafi/conposetestapp/home/Favorite.kt
@@ -1,8 +1,20 @@
 
 package com.shafi.conposetestapp.home
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.shafi.conposetestapp.R
 import com.shafi.conposetestapp.ui.theme.RecipeScaffold
 
 @Composable
@@ -22,6 +34,21 @@ fun Favorite(
         },
         modifier = modifier
     ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxSize()
+                .wrapContentSize()
+                .padding(24.dp)
+                .padding(it)
+        ) {
+            Text(
+                text = stringResource(R.string.work_in_progress),
+                style = MaterialTheme.typography.subtitle2,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
 
     }
 }
--- app/src/main/java/com/shafi/conposetestapp/home/Feed.kt
+++ app/src/main/java/com/shafi/conposetestapp/home/Feed.kt
@@ -10,15 +10,20 @@ import androidx.compose.animation.shrinkVertically
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.add
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.windowInsetsTopHeight
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -27,8 +32,11 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.shafi.conposetestapp.R
 import com.shafi.conposetestapp.ui.theme.RecipeScaffold
 
 @Composable
@@ -48,6 +56,21 @@ fun Feed(
         },
         modifier = modifier
     ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxSize()
+                .wrapContentSize()
+                .padding(24.dp)
+                .padding(it)
+        ) {
+            Text(
+                text = stringResource(R.string.work_in_progress),
+                style = MaterialTheme.typography.subtitle1,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
 
     }
 }
