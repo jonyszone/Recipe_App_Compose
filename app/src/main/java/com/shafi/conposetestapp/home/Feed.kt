ea0c611361df0348cf2393d9ef8ebc30e5148e4c
Shafi <jonyszone@gmail.com>
2023-11-27 23:11:11 +0600

    a list of food showed in homepage

--- app/src/main/java/com/shafi/conposetestapp/home/Feed.kt
+++ app/src/main/java/com/shafi/conposetestapp/home/Feed.kt
@@ -1,42 +1,28 @@
 
 package com.shafi.conposetestapp.home
 
-import android.content.res.Configuration
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.expandVertically
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
-import androidx.compose.animation.shrinkVertically
-import androidx.compose.animation.slideInVertically
-import androidx.compose.animation.slideOutVertically
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.add
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.statusBars
-import androidx.compose.foundation.layout.windowInsetsTopHeight
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
 import com.shafi.conposetestapp.R
+import com.shafi.conposetestapp.component.ItemFoodCard
+import com.shafi.conposetestapp.model.FakeFoodData
+import com.shafi.conposetestapp.model.FoodRecipe
 import com.shafi.conposetestapp.ui.theme.RecipeScaffold
 
 @Composable
@@ -61,7 +47,7 @@ fun Feed(
             modifier = Modifier
                 .fillMaxSize()
                 .wrapContentSize()
-                .padding(24.dp)
+                .padding(4.dp)
                 .padding(it)
         ) {
             Text(
@@ -70,8 +56,32 @@ fun Feed(
                 textAlign = TextAlign.Center,
                 modifier = Modifier.fillMaxWidth()
             )
+            
+            Spacer(modifier = Modifier)
+            Home(foodList = FakeFoodData.foodList) {
+
+            }
         }
 
+    }
+}
+
+@Composable
+fun Home(/*navController: NavHostController,*/ foodList: List<FoodRecipe>, toggleTheme: () -> Unit) {
+    LazyColumn {
+        item {
+           // Spacer(modifier = Modifier.height(3.dp))
+        }
+        items(foodList) {
+            foodList.forEach {
+                ItemFoodCard(
+                    it,
+                    onItemClicked = { food ->
+
+                    }
+                )
+            }
+        }
     }
 }
 
---
+++ app/src/main/java/com/shafi/conposetestapp/model/FakeFoodData.kt
@@ -0,0 +1,29 @@
+package com.shafi.conposetestapp.model
+
+import com.shafi.conposetestapp.R
+
+object FakeFoodData {
+    val foodList = listOf(
+        FoodRecipe(
+            0,
+            "Fried chicken",
+            R.drawable.fried_chiken,
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,",
+
+        ),
+        FoodRecipe(
+            1,
+            "Bakso Mastom",
+            R.drawable.bakso,
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,",
+
+        ),
+        FoodRecipe(
+            2,
+            "Sour Crab",
+            R.drawable.crab,
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,",
+
+        )
+    )
+}
